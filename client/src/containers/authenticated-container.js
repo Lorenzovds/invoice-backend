d58af41335b19970d30b29ccd8abad48ffc6e9f7
Lorenzo Van De Sijpe <lorenzo.vandesijpe@hotmail.nl>
2018-03-03 10:48:35 +0100

    add main body, header and sidebar

--- client/src/containers/authenticated-container.js
+++ client/src/containers/authenticated-container.js
@@ -1,17 +1,100 @@
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router'
-import '../App.css'
 import InvoicesContainer from './invoices-container'
+import {Header, Sidebar, Grid,
+  Segment, Menu, Icon, Button } from 'semantic-ui-react'
+
+import '../App.css'
 
 class AuthenticatedContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      sidebarVisible: false,
+      sidebarButton: 'angle double right'
+    }
+  }
+
   render () {
+    const { sidebarVisible } = this.state
     return (
-      <div>
-        <h2>MAIN PAGE</h2>
+      <div style={{ height: '100vh' }}>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar as={Menu}
+            visible={sidebarVisible}
+            animation='slide along'
+            width='thin'
+            icon='labeled'
+            vertical
+            inverted>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher onClick={() => this.handleMainBodyClick()}>
+            <Grid columns='equal'>
+              <Grid.Row>
+                <Grid.Column width={1}>
+                  <Button
+                    onClick={() => this.toggleSidebar()}
+                    icon={this.state.sidebarButton}
+                    style={{marginTop: '50vh'}}
+                    floated='left' />
+                </Grid.Column>
+                <Grid.Column>
+                  <Grid columns='equal'>
+                    <Grid.Row>
+                      <Grid.Column>
+                        { this.renderHeader() }
+                      </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                      <Grid.Column>
+                        { this.getRouter() }
+                      </Grid.Column>
+                    </Grid.Row>
+                  </Grid>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    )
+  }
+
+  handleMainBodyClick () {
+    const { sidebarVisible } = this.state
+    if (sidebarVisible) this.toggleSidebar(false)
+  }
+
+  toggleSidebar (state) {
+    const newState = state || !this.state.sidebarVisible
+    this.setState({ sidebarVisible: newState })
+    this.setState({ sidebarButton: newState ? 'angle double left' : 'angle double right' })
+  }
+
+  renderHeader () {
+    return (
+      <Segment basic style={{ marginTop: '2em' }}>
+        <Header as='h2' icon textAlign='center'>
+          <Icon name='book' circular />
+          <Header.Content>
+            Facturatie programma
+          </Header.Content>
+        </Header>
+      </Segment>
+    )
+  }
+
+  getRouter () {
+    return (
+      <Segment raised>
         <Switch>
           <Route path='/invoices' component={InvoicesContainer} />
         </Switch>
-      </div>
+      </Segment>
     )
   }
 }
