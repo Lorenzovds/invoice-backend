23d221b5d0b573495ccfebfd77f9cc13132f4cab
Lorenzo Van De Sijpe <lorenzo.vandesijpe@hotmail.nl>
2018-03-24 14:09:14 +0100

    allow cors

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  next()
+})
+
 app.use('/api', router)
 
 app.use('/', express.static(`${__dirname}/client/build`))
