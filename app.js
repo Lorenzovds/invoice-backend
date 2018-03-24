c9fe816a26a74bacfc18ae1421c864510668a0d7
Lorenzo Van De Sijpe <lorenzo.vandesijpe@hotmail.nl>
2018-03-24 16:22:35 +0100

    final fix express route (hopefully)

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/api', router)
 
+app.use('/', express.static(`${__dirname}/client/build`))
 app.use('*', express.static(`${__dirname}/client/build`))
 
 // catch 404 and forward to error handler
