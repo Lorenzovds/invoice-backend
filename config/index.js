33569dc15093e2ba38944763789af57b2126cffa
lorenzovds <lorenzo.vandesijpe@hotmail.nl>
2018-10-30 10:01:39 +0100

    allow values to be passed trough heroku

---
+++ config/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+let credentials
+// use hardcoded values for local development
+if (fs.existsSync(path.resolve(`${__dirname}/config.js`))) {
+  credentials = require(path.resolve(`${__dirname}/config`))
+} else {
+  credentials = process.env
+}
+
+const { orgUrl, token, mongoDB } = credentials
+
+module.exports = {
+  orgUrl: orgUrl,
+  token: token,
+  mongoDB: mongoDB,
+  users: require('./users.js')
+}
