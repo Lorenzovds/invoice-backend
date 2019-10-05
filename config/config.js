8124376eed874cde97108dcc8bc0aa3a5a81d7cc
lorenzovds <lorenzo.vandesijpe@hotmail.nl>
2019-10-05 11:35:02 +0200

    chore: upload config since this is a private repo

---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  orgUrl: 'https://dev-777161.oktapreview.com',
+  token: '00LbZjiH9lLfN1GVDpg52p9TLLezieMA6oifNh6QQf',
+  mongoDB: 'mongodb://app:app@ds121889.mlab.com:21889/invoicer'
+}
